795e0a35a3d7821fea1fdcc573b157851621e629
DBarry-Code <dabrowski.barry@gmail.com>
2021-11-03 19:17:43 +0100

    new sql table include

--- server/setup.sql
+++ server/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS pwreset;
 
 CREATE TABLE users (
     id              SERIAL PRIMARY KEY,
@@ -6,5 +7,11 @@ CREATE TABLE users (
     last_name       VARCHAR(255) NOT NULL,
     email           VARCHAR(50) NOT NULL UNIQUE,
     password_hash   VARCHAR NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE pwreset (
+    email           VARCHAR(50) NOT NULL,
+    code            VARCHAR(255) NOT NULL,
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
